8b083f8ab606c7b6e8129944b8e9fe383fd4f6c7
Tato Corp <pavoro30@gmail.com>
2024-11-18 18:27:54 -0400

    115200 baud rate

--- include/diffdrive_arduino/config.h
+++ include/diffdrive_arduino/config.h
@@ -9,8 +9,8 @@ struct Config
   std::string left_wheel_name = "left_wheel";
   std::string right_wheel_name = "right_wheel";
   float loop_rate = 30;
-  std::string device = "/dev/ttyACM0";
-  int baud_rate = 57600;
+  std::string device = "/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0";
+  int baud_rate = 115200; //57600
   int timeout = 1000;
   int enc_counts_per_rev = 90;
 };
